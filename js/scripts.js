caa42d8c47386e83b984e5691a3c2ead8940fa60
Wakil Muzeyen <wekilmuzeyen@gmail.com>
2024-12-16 17:55:46 -0600

    put all pages into one, update email, UI and many more

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
 
+  // Toggle dropdown menu
   hamburger.addEventListener('click', () => {
     navMenu.classList.toggle('active');
+  });
+
+  // Optional: Close menu when clicking outside
+  document.addEventListener('click', (event) => {
+    if (!hamburger.contains(event.target) && !navMenu.contains(event.target)) {
+      navMenu.classList.remove('active');
+    }
   });
 });
